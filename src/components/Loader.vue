e21875026d41fcb0696f5b69c49a342ea47c857f
Abdelhalim Bouguedra <abdelhalim.bouguedra@emploitic.com>
2019-05-22 00:04:29 +0100

    Add Loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,18 @@
+<template>
+  <Overlay>
+    <Spinner></Spinner>
+  </Overlay>
+</template>
+
+<script>
+import Overlay from '@/components/Overlay';
+import Spinner from '@/components/Spinner';
+
+export default {
+  name: 'Loader',
+  components: {
+    Overlay,
+    Spinner
+  }
+}
+</script>
