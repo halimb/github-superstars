4efa08c6296f5fdb2ee83ec19dfdc7cf7969390e
Abdelhalim Bouguedra <abdelhalim.bouguedra@emploitic.com>
2019-05-21 23:30:04 +0100

    Remove unused imports from main.js

--- src/main.js
+++ src/main.js
@@ -1,15 +1,8 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import App from './App.vue'
+import App from '@/App.vue'
 
-Vue.use(Vuex)
-Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
-
-Vue.use(store)
