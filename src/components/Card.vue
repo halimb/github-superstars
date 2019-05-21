65c1026f024c102aeaca45bee05dbc4038d42f60
Abdelhalim Bouguedra <abdelhalim.bouguedra@emploitic.com>
2019-05-21 23:28:37 +0100

    Add Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="f align-center">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
