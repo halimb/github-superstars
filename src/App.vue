899feac3611c089dd5800d621c99077a890f38fa
Abdelhalim Bouguedra <abdelhalim.bouguedra@emploitic.com>
2019-05-21 22:06:28 +0100

    Add ReposList component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ReposList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ReposList from '@/components/ReposList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ReposList
   }
 }
 </script>
 
 <style lang="scss">
-    #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-    background: $primary;
-    }
 </style>
