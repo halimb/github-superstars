27de316fc57253aa081f2945de873b478c4163e9
Abdelhalim Bouguedra <abdelhalim.bouguedra@emploitic.com>
2019-05-23 01:11:30 +0100

    Add global mixins StaticAssets and ThemeData

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+import ThemeData from '@/mixins/ThemeData';
+import StaticAssets from '@/mixins/StaticAssets';
 import App from '@/App.vue'
 
 Vue.config.productionTip = false
+
+// Add global mixins
+Vue.mixin(ThemeData);
+Vue.mixin(StaticAssets);
 
 new Vue({
   render: h => h(App),
