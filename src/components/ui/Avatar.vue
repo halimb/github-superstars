e3f555a1e7c0726372bef2c129329202b8dcb1bc
Abdelhalim Bouguedra <abdelhalim.bouguedra@emploitic.com>
2019-05-23 02:33:32 +0100

    Handle Avatar styles based on image orientation

--- src/components/ui/Avatar.vue
+++ src/components/ui/Avatar.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="avatar">
-    <img :src="image || getImage('github.svg')" alt="">
+  <div 
+    class="avatar"
+    :style="{
+      height: `${size}px`,
+      width: `${size}px`
+    }"
+    >
+    <div class="avatar-img">
+      <img 
+        alt="avatar"
+        ref="imageElement"
+        @load="onImageLoad"
+        :src="image || getImage('github.svg')"
+        :class="imageClass"
+      >
+    </div>
   </div>
 </template>
 
@@ -10,10 +24,43 @@ export default {
   props: {
     image: {
       type: String,
+    },
+    size: {
+      type: Number,
+      default: 50
+    }
+  },
+  computed: {
+    imageClass() {
+      return `avatar-img--${this.isLandscape ? 'landscape' : 'portrait'}`
+    }
+  },
+  methods: {
+    onImageLoad() {
+      let imageElement = this.$refs.imageElement;
+      this.isLandscape = imageElement.width > imageElement.height;
     }
   }
 }
 </script>
 
 <style lang="scss">
+.avatar {
+  display: inline-block;
+  border: 1px solid black;
+  .avatar-img {
+    display:block;
+    overflow: hidden;
+    border-radius: 1000px;
+    max-width: unset;
+    &--portrait {
+      width: 100%;
+      height: auto;
+    }
+    &--landscape {
+      width: auto;
+      height: 100%;
+    }
+  }
+}
 </style>
