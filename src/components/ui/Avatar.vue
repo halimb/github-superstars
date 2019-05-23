cb33d02d029f826b7b0ff28a7d915333b46391e3
Abdelhalim Bouguedra <abdelhalim.bouguedra@emploitic.com>
2019-05-23 01:13:09 +0100

    Add component Avatar

---
+++ src/components/ui/Avatar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="avatar">
+    <img :src="image || getImage('github.svg')" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Avatar',
+  props: {
+    image: {
+      type: String,
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
